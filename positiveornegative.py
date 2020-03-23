79a0be53b6307f2aa3cf0708d6050946ec45e709
Kaushal M. Jain <55205377+coder2000-kmj@users.noreply.github.com>
2020-03-23 16:04:43 +0530

    Add files via upload

---
+++ positiveornegative.py
@@ -0,0 +1,6 @@
+#To check whether the number is positive using simple if 
+num = int(input('Enter a number: '))
+if num>0:
+    print("THE GIVEN NUMBER IS POSITIVE")
+if num<0:
+    print("THE GIVEN NUMBER IS NEGATIVE")
