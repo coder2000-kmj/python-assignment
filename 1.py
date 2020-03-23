791fe8f7aad2dbe90bbb709e7c08ad0d001470d3
Kaushal M. Jain <55205377+coder2000-kmj@users.noreply.github.com>
2020-03-23 15:58:54 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+#To read two integers and find the sum , diff , mult and div 
+a = int(input('Enter a number: '))
+b = int(input('Enter another number '))
+add =a+b
+diff =a-b
+mult= a*b
+div=a/b
+print("sum= ",add)
+print("difference= ",diff)
+print("multiplication= ",mult)
+print("quotient= ",div)
