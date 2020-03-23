8082e54d18036ffd146f8a49e573204b64987802
Kaushal M. Jain <55205377+coder2000-kmj@users.noreply.github.com>
2020-03-23 16:02:18 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,8 @@
+#To find the simple interest for a given value P,T and R.  The program must take the input from the user. 
+P=int(input("ENTER THE PRINCIPLE AMOUNT"))
+T=int(input("ENTER THE TIME IN SCALE OF MONTHS"))
+R=int(input("ENTER THE RATE OF INTREST "))
+si=(P*T*R)/100
+print("SIMPLE INTREST =",si)
+
+
